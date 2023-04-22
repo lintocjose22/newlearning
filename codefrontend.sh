142cea5f87b3a47aeda1fc7a4520706f182aaed5
lintocjose22 <lintocjose22@gmail.com>
2023-04-22 16:35:52 -0400

    front end

--- codefrontend.sh
+++ codefrontend.sh
@@ -6,12 +6,12 @@ yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
-#sudo cp $script_Path/roboshop.conf /etc/nginx/default.d/
+sudo cp $script_Path/roboshop.conf /etc/nginx/default.d/
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 echo $pwd
 unzip /tmp/frontend.zip
-sudo cp $script_Path/roboshop.conf /etc/nginx/default.d/
+#sudo cp $script_Path/roboshop.conf /etc/nginx/default.d/
 useradd ${user_name}
 sed -i -e 's|/catalogue/ { proxy_pass http://localhost:|/catalogue/ { proxy_pass http://frontend.lintocjose.online:|' /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
