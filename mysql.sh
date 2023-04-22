068209bcf73833f78b8568d249e5ed0fb62e63cd
lintocjose22 <lintocjose22@gmail.com>
2023-04-22 17:21:59 -0400

    front end

--- mysql.sh
+++ mysql.sh
@@ -2,10 +2,17 @@
 real_path=$(realpath "$0")
 echo $real_path
 script_Path=$(dirname "$real_path")
+echo script_Path
 dnf module disable mysql -y
+echo sql installed
 sudo cp $script_Path/mysql.repo /etc/nginx/default.d/
+echo file copied
 yum install mysql-community-server -y
+echo mysql installed
 systemctl enable mysqld
 systemctl start mysql
+echo services started
 mysql_secure_installation --set-root-pass RoboShop@1
+hange the default root password i
 mysql -uroot -pRoboShop@1
+completed
