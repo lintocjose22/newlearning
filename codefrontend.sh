5f3faf36e312c74f7bd5252c46947bcb7951e571
lintocjose22 <lintocjose22@gmail.com>
2023-04-22 13:27:59 -0400

    front end

--- codefrontend.sh
+++ codefrontend.sh
@@ -8,7 +8,10 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
+echo $pwd
 unzip /tmp/frontend.zip
+script_Path=$(dirname $0)
+echo $script_Path
 sudo cp $script_Path/roboshop.conf /etc/nginx/default.d/
 useradd ${user_name}
 sed -i -e 's|/catalogue/ { proxy_pass http://localhost:|/catalogue/ { proxy_pass http://frontend.lintocjose.online:|' /etc/nginx/default.d/roboshop.conf
