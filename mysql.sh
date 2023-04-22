30912d4e36231270eccd65ffb717e6993b504673
lintocjose22 <lintocjose22@gmail.com>
2023-04-22 17:30:59 -0400

    front end

--- mysql.sh
+++ mysql.sh
@@ -2,10 +2,12 @@
 real_path=$(realpath "$0")
 echo $real_path
 script_Path=$(dirname "$real_path")
-echo $script_Path
+echo scriptpath is  $script_Path
+#exit
+source ${script_Path}/common.sh
 dnf module disable mysql -y
 echo sql installed
-sudo cp $script_Path/mysql.repo /etc/nginx/default.d/
+cp $script_Path/mysql.repo /etc/nginx/default.d/
 echo file copied
 yum install mysql-community-server -y
 echo mysql installed
